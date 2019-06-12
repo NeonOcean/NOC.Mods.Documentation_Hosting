12c479b413639fff4593f8369279ec9d18ddcc91
NeonOcean <40919586+NeonOcean@users.noreply.github.com>
2019-06-12 16:19:33 -0500

    6/12/2019 Update

--- indexing/structure_indexing.js
+++ indexing/structure_indexing.js
@@ -4,18 +4,110 @@ var StructureIndexing = [
 		"StartOpened": true,
 		"Entries": [
 			{
-				"Name": "NeonOcean.Main",
+				"Name": "Debug",
+				"Path": "s4/debug/index.html",
+				"StartOpened": false,
+				"Entries": [
+					{
+						"Name": "General",
+						"StartOpened": false,
+						"Entries": [
+							{
+								"Name": "Installation",
+								"Path": "s4/debug/general/installation/index.html",
+								"StartOpened": false
+							},
+							{
+								"Name": "Reporting Bugs",
+								"Path": "s4/debug/general/reporting-bugs/index.html",
+								"StartOpened": false
+							}
+						]
+					}
+				]
+			},
+			{
+				"Name": "Main",
 				"Path": "s4/main/index.html",
 				"StartOpened": false,
 				"Entries": [
 					{
 						"Name": "General",
-						"Path": "s4/main/general/index.html",
 						"StartOpened": false,
 						"Entries": [
 							{
 								"Name": "Installation",
-								"Path": "s4/main/general/installation.html",
+								"Path": "s4/main/general/installation/index.html",
+								"StartOpened": false
+							},
+							{
+								"Name": "Reporting Bugs",
+								"Path": "s4/main/general/reporting-bugs/index.html",
+								"StartOpened": false
+							}
+						]
+					},
+					{
+						"Name": "Settings",
+						"StartOpened": false,
+						"Entries": [
+							{
+								"Name": "Check For Preview Updates",
+								"Path": "s4/main/settings/check-for-preview-updates/index.html",
+								"StartOpened": false
+							},
+							{
+								"Name": "Check For Updates",
+								"Path": "s4/main/settings/check-for-updates/index.html",
+								"StartOpened": false
+							},
+							{
+								"Name": "Show Promotions",
+								"Path": "s4/main/settings/show-promotions/index.html",
+								"StartOpened": false
+							}
+						]
+					}
+				]
+			},
+			{
+				"Name": "Order",
+				"Path": "s4/order/index.html",
+				"StartOpened": false,
+				"Entries": [
+					{
+						"Name": "General",
+						"StartOpened": false,
+						"Entries": [
+							{
+								"Name": "Installation",
+								"Path": "s4/order/general/installation/index.html",
+								"StartOpened": false
+							},
+							{
+								"Name": "Reporting Bugs",
+								"Path": "s4/order/general/reporting-bugs/index.html",
+								"StartOpened": false
+							}
+						]
+					},
+					{
+						"Name": "Settings",
+						"StartOpened": false,
+						"Entries": [
+							{
+								"Name": "Check For Preview Updates",
+								"Path": "s4/order/settings/check-for-preview-updates/index.html",
+								"StartOpened": false
+							},
+							{
+								"Name": "Check For Updates",
+								"Path": "s4/order/settings/check-for-updates/index.html",
+								"StartOpened": false
+							},
+							{
+								"Name": "Show Promotions",
+								"Path": "s4/order/settings/show-promotions/index.html",
 								"StartOpened": false
 							}
 						]
